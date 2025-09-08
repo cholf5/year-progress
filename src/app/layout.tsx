d1bf569403f3bf1646c91db6065e511ec2bdb7b0
cholf5 <cholf5@hotmail.com>
2025-09-09 01:44:00 +0800

    feat: 添加多种图标和更新 manifest，支持不同设备的图标显示

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,7 +82,9 @@ export default function RootLayout({
         <meta name="theme-color" content="#000000" />
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="YearProgress.org" />
